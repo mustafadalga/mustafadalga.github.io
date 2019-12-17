29312ab1f8cf2e31526f4d796460224c86cc2e21
Mustafa Dalğa <mustafadalgaa@gmail.com>
2019-12-17 20:32:37 +0300

    mustafadalga.github.io Template Update and Adding New Designs

---
+++ Front-End-Developments/Mizuxe/assets/js/js.js
@@ -0,0 +1,24 @@
+$('#year').text(new Date().getFullYear());
+
+//Init Scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+
+//Smooth Scrolling
+
+$('#main-nav a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
