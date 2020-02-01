4f3ac94563b51e176b70e54e6c582eadef9c39d7
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-02-01 20:30:41 +0300

    PWA

--- Front-End-Developments/PWA/js/app.js
+++ Front-End-Developments/PWA/js/app.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", showCoffees);
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("js/serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
